c989951cb65306184faf235a06805d5d344aad49
David Scott Kirby <david@ecomatics.net>
2013-12-18 18:58:30 -0900

    Optimizations

--- js/gielis-supershape3d.js
+++ js/gielis-supershape3d.js
@@ -1,171 +1,130 @@
-if (!Modernizr.webgl) {
-	alert("You're browser does not support WebGL :(");
-} else {
 
-	var viewX = viewportSize.getWidth(),
-		viewY = viewportSize.getHeight(),
-		viewHalfX = viewX/2.0,
-		viewHalfY = viewY/2.0;
-		
-	var mouseX,
-		mouseY,
-		camera, 
-		scene,
-		renderer,
-		container,		
-		controls,
-		line,
-		lineGeometry;
-		
-	var π  = Math.PI,
-		τ  = 2.0*π,
-		vθ = 50.0,
-		vφ = 50.0,
-		dθ = τ/vθ,
-		dφ = π/vφ,
-		θ  = 0.0,
-		φ  = 0.0,		
-		m =  randomFloatBetween( 1.0, 27.0, 2),
-		a =  randomFloatBetween( 1.0, 5.0, 2),
-		b =  randomFloatBetween( 1.0, 5.0, 2),
-		n1 = randomFloatBetween( 0.0, 4.0, 2),
-		n2 = randomFloatBetween( 0.0, 4.0, 2),
-		n3 = randomFloatBetween( 0.0, 4.0, 2),
-		ρ1 = 0.0,
-		ρ2 = 0.0,
-		ρx1 = 0.0,
-		ρy1 = 0.0,
-		ρx2 = 0.0,
-		ρy2 = 0.0;
-		
-	init();
-	setupControls();
-	animate();
+function randomFloatBetween(minValue, maxValue, precision) {
+    if (typeof (precision) == 'undefined') {
+        precision = 2;
+    }
+    return parseFloat(Math.min(minValue + (Math.random() * (maxValue - minValue)), maxValue).toFixed(precision));
 }
 
-function setupControls() {
+var m = randomFloatBetween(1.0, 27.0, 2),
+    a = randomFloatBetween(1.0, 5.0, 2),
+    b = randomFloatBetween(1.0, 5.0, 2),
+    n1 = randomFloatBetween(0.0, 4.0, 2),
+    n2 = randomFloatBetween(0.0, 4.0, 2),
+    n3 = randomFloatBetween(0.0, 4.0, 2);
 
-	controls = new THREE.TrackballControls(camera);
+var lineGeometry = new THREE.Geometry();
 
-	controls.rotateSpeed = 0.5;
-	controls.zoomSpeed = 4.0;
-	controls.noZoom = false;
-	controls.noPan = true;
-	controls.noRotate = false;
-	controls.staticMoving = false;
-	controls.dynamicDampingFactor = .15;
-}
+var line = new THREE.Line(
+        lineGeometry, new THREE.LineBasicMaterial({
+            color: 0x013f8f,
+            opacity: 0.5,
+            blending: THREE.AdditiveBlending,
+            transparent: true
+        })
+    );
+
+var mouseX, mouseY, camera, scene, renderer, controls;
+
+init();
 
 function init() {
 
-	container = document.createElement('div');
-	
-	document.body.appendChild(container);
+    container = document.createElement('div');
 
-	camera = new THREE.PerspectiveCamera(75, viewX/viewY, 0.0001, 10000);
-	camera.position.z = 5;
-		
-	scene = new THREE.Scene();
-	renderer = new THREE.WebGLRenderer();
-	renderer.setSize(viewX, viewY);	
-	
-	container.appendChild(renderer.domElement);
-				
-	setupItems();
+    document.body.appendChild(container);
+
+    camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.0001, 10000);
+    camera.position.z = 5;
+    camera.position.y = Math.random();
+    scene = new THREE.Scene();
+    renderer = new THREE.WebGLRenderer();
+    renderer.setSize(window.innerWidth, window.innerHeight);
+    container.appendChild(renderer.domElement);
+    document.addEventListener('mousemove', onDocumentMouseMove, false);
+    document.addEventListener('touchstart', onDocumentTouchStart, false);
+    document.addEventListener('touchmove', onDocumentTouchMove, false);
+    controls = new THREE.TrackballControls(camera);
+    controls.rotateSpeed = 0.5;
+    controls.zoomSpeed = 1.0;
+    controls.noZoom = false;
+    controls.noPan = true;
+    controls.noRotate = false;
+    controls.staticMoving = false;
+    controls.dynamicDampingFactor = .2;
+    updateVertices();
+    animate();
 }
 
-function setupItems() {
-	
-	lineGeometry = new THREE.Geometry();
-	
-	θ = -τ;
-	
-	while (θ <= τ)
-	{
-		φ = -π;
-		
-		ρx1 = Math.pow(Math.abs(Math.cos(m*θ/2))/a, n2);
-		ρy1 = Math.pow(Math.abs(Math.sin(m*θ/2))/b, n3);
-		
-		ρ1 = Math.pow(ρx1 + ρy1, -1/n1);
-		
-		while (φ <= π)
-		{			
-			ρx2 = Math.pow(Math.abs(Math.cos(m*φ/2))/a, n2);
-			ρy2 = Math.pow(Math.abs(Math.sin(m*φ/2))/b, n3);			
-			
-			ρ2 = Math.pow(ρx2 + ρy2, -1/n1);
-					
-			lineGeometry.vertices.push(new THREE.Vector3(
-				ρ1 * Math.cos(θ) * ρ2 * Math.cos(φ),
-				ρ1 * Math.sin(θ) * ρ2 * Math.cos(φ),
-				ρ2 * Math.sin(φ)
-			));
-			
-        	φ += dφ;
-		}
-		
-		θ += dθ;
-	}
-	
-	line = new THREE.Line(
-		lineGeometry, new THREE.LineBasicMaterial({
-			color : 0x003f8f,
-			opacity : 0.5,
-			blending: THREE.AdditiveBlending , 
-			transparent: true
-		})
-	);
-		
-	scene.add(line);	
-	
-	document.addEventListener('mousemove',  onDocumentMouseMove,  false);
-	document.addEventListener('touchstart', onDocumentTouchStart, false);
-	document.addEventListener('touchmove',  onDocumentTouchMove,  false);
+function updateVertices() {
+
+    var theta = -(2 * Math.PI), p1, p2;
+
+    while (theta <= (2 * Math.PI)) {
+
+        var phi = -Math.PI;
+
+        var px1 = Math.pow(Math.abs(Math.cos(m * theta / 2)) / a, n2);
+        var py1 = Math.pow(Math.abs(Math.sin(m * theta / 2)) / b, n3);
+
+        p1 = Math.pow(px1 + py1, -1 / n1);
+
+        while (phi <= Math.PI) {
+
+            var px2 = Math.pow(Math.abs(Math.cos(m * phi / 2)) / a, n2);
+            var py2 = Math.pow(Math.abs(Math.sin(m * phi / 2)) / b, n3);
+
+            p2 = Math.pow(px2 + py2, -1 / n1);
+
+            lineGeometry.vertices.push(
+                new THREE.Vector3(
+                    (p1 * Math.cos(theta) * p2 * Math.cos(phi)),
+                    (p1 * Math.sin(theta) * p2 * Math.cos(phi)),
+                    (p2 * Math.sin(phi))));
+
+            phi += 0.1;
+        }
+
+        theta += 0.1;
+    }
+
+    scene.add(line);
 }
 
 function onDocumentMouseMove(event) {
 
-	mouseX = event.clientX - viewHalfX;
-	mouseY = event.clientY - viewHalfY;
+    mouseX = event.clientX - window.innerWidth / 2.0;
+    mouseY = event.clientY - window.innerHeight / 2.0;
 }
 
 function onDocumentTouchStart(event) {
 
-	if (event.touches.length > 1) {
+    if (event.touches.length > 1) {
 
-		event.preventDefault();
+        event.preventDefault();
 
-		mouseX = event.touches[0].pageX - viewHalfX;
-		mouseY = event.touches[0].pageY - viewHalfY;
-	}
+        mouseX = event.touches[0].pageX - window.innerWidth / 2.0;
+        mouseY = event.touches[0].pageY - window.innerHeight / 2.0;
+    }
 }
 
 function onDocumentTouchMove(event) {
 
-	if (event.touches.length == 1) {
+    if (event.touches.length == 1) {
 
-		event.preventDefault();
+        event.preventDefault();
 
-		mouseX = event.touches[0].pageX - viewHalfX;
-		mouseY = event.touches[0].pageY - viewHalfY;
-	}
+        mouseX = event.touches[0].pageX - window.innerWidth / 2.0;
+        mouseY = event.touches[0].pageY - window.innerHeight / 2.0;
+    }
+
 }
 
 function animate() {
-	requestAnimationFrame(animate);
-	controls.update();
-	
-	render();
+    controls.update();
+    renderer.render(scene, camera);
+    requestAnimationFrame(animate);
 }
 
 function render() {
-	renderer.render( scene, camera );
-}
-
-function randomFloatBetween(minValue, maxValue, precision) {
-    if(typeof(precision) == 'undefined') {
-        precision = 2;
-    }
-    return parseFloat(Math.min(minValue + (Math.random() * (maxValue - minValue)),maxValue).toFixed(precision));
 }
